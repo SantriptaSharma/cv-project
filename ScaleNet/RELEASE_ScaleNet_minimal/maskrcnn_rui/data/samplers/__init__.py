da1c90934d80643cbb8e319dd5ee06666beca2eb
SantriptaSharma <santripta@gmail.com>
2024-12-03 23:08:50 +0530

    remove: scalenet

--- ScaleNet/RELEASE_ScaleNet_minimal/maskrcnn_rui/data/samplers/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from .distributed import DistributedSampler
-
-__all__ = ["DistributedSampler"]
-
--- ScaleNet/RELEASE_ScaleNet_minimal/models/pointnet/nn/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .conv import Conv1d, Conv2d
-from .linear import FC
-from .mlp import MLP, SharedMLP
